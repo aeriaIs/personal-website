574430bbc20ffaa37b9e4ae4bf799a9c0c63d63a
aerials <nosvengeance@gmail.com>
2025-03-07 14:10:48 +0700

    config: update all configs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: true
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  }
 };
 
 export default nextConfig;
